e04714bdf9d6be89032bbbcfc7a1a1e971e8a16f
avoss19 <31517943+avoss19@users.noreply.github.com>
2018-11-07 23:18:57 -0600

    fix

--- src/drive.py
+++ src/drive.py
@@ -6,26 +6,17 @@ from geometry_msgs.msg import Twist
 
 from pyax12.connection import Connection
 
-# class driveAx:
-
-# def __init__(self, l = (1, 2), r = (3, 4), device='/dev/ttyACM0', baud=1000000):
-#     self.sc = Connection(port=device, baudrate=baud)
-#     self.L = l
-#     self.R = r
-#     for i in (l + r):
-#         __set_continuous(i)
-
-l = (1, 2)
-r = (3, 4)
+L = (1, 2)
+R = (3, 4)
 
 device = '/dev/ax'
 buad = 1000000
 
-def __set_continuous(self, motor_id):
-    self.sc.set_cw_angle_limit(motor_id, 0, degrees=False)
-    self.sc.set_ccw_angle_limit(motor_id, 0, degrees=False)
+def set_continuous(motor_id):
+    sc.set_cw_angle_limit(motor_id, 0, degrees=False)
+    sc.set_ccw_angle_limit(motor_id, 0, degrees=False)
 
-def __speedConvert(self, speed):
+def speedConvert(speed):
     if(speed > 0.0):
         speed = 1024 + speed * 1023
         return speed
@@ -36,17 +27,17 @@ def __speedConvert(self, speed):
         speed = 0
         return speed
 
-def forward(self, speed):
-    for i in (self.L + self.R):
-        self.sc.set_speed(i, __speedConvert(speed))
+def forward(speed):
+    for i in (L + R):
+        sc.set_speed(i, speedConvert(speed))
 
-def left(self, speed):
-    for i in (self.L):
-        self.sc.set_speed(i, __speedConvert(speed))
+def left(speed):
+    for i in (L):
+        sc.set_speed(i, speedConvert(speed))
 
-def right(self, sspeed):
-    for i in (self.R):
-        self.sc.set_speed(i, __speedConvert(speed))
+def right(speed):
+    for i in (R):
+        sc.set_speed(i, speedConvert(speed))
 
 # drive = driveAx()
 
@@ -63,10 +54,12 @@ def callback(msg):
     rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
     drive(msg.linear.x, msg.angular.z)
 
-def listener()):
+def listener():
     rospy.init_node('cmd_vel_listener')
-    rospy.Subscriber("/cmd_vel", Twist, self.callback)
+    rospy.Subscriber("/cmd_vel", Twist, callback)
     rospy.spin()
 
 if __name__ == '__main__':
+    for i in (L + R):
+        set_continuous(i)
     listener()
